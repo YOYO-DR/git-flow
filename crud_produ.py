0891df4e997bd5f8fa0ffa9457ffc445c284e9da
alejandra <afajardo04@misena.edu.co>
2023-02-16 18:59:43 -0500

    creacion de la funcion de consultar productos

--- crud_produ.py
+++ crud_produ.py
@@ -2,3 +2,32 @@ from openpyxl import load_workbook
 from datetime import datetime
 
 rut=r"hojaDatos.xlsx"
+
+def consultar_producto(rut:str,extraer:str):
+    Archivo_Exccel=load_workbook(rut)
+    Hoja_datos=Archivo_Exccel["productos"]
+    Hoja_datos=Hoja_datos["A2":"E"+str(Hoja_datos.max_row)]
+
+    info={}
+
+    for i in Hoja_datos:
+        if isinstance(i[0].value, int):
+            info.setdefault(i[0].value,{"nombre":i[1].value, "categoria":i[2].value, "precio":i[3].value, "cantidad":i[4].value})
+
+    if not(extraer=="todo"):
+        info=filtrar(info, extraer)
+
+    for i in info:
+        print("****Productos****")
+        print("id: "+ str(i)+"\n"+"Nombre: "+str(info[i]["nombre"])+"\n"+"Categoria: "+str(info[i]["categoria"])+"\n"+"Precio: "+str(info[i]["precio"])+"\n"+"Cantidad: "+str(info[i]["cantidad"]))
+        print()
+
+    return
+
+def filtrar(info:dict,filtro:str):
+    aux={}
+
+    for i in info:
+        if info[i]["categoria"]==filtro:
+            aux.setdefault(i,info[i])
+    return aux
