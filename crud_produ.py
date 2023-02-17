9257ce3535406bf66640ff12c41fdf09356a5436
alejandra <afajardo04@misena.edu.co>
2023-02-16 21:23:37 -0500

    creacion de la funcion de agregar productos

--- crud_produ.py
+++ crud_produ.py
@@ -2,3 +2,26 @@ from openpyxl import load_workbook
 from datetime import datetime
 
 rut=r"hojaDatos.xlsx"
+
+def agregar_producto(rut:int,datos:dict):
+    Archivo_Exccel=load_workbook(rut)
+    Hoja_datos=Archivo_Exccel["productos"]
+    Hoja_datos=Hoja_datos["A2":"E"+str(Hoja_datos.max_row+1)]
+    hoja=Archivo_Exccel.active
+
+    nombre=2
+    categoria=3
+    precio=4
+    cantidad=5
+    for i in Hoja_datos:
+
+        if not(isinstance(i[0].value, int)):
+            identificador=i[0].row
+            hoja.cell(row=identificador, column=1).value=identificador-1
+            hoja.cell(row=identificador, column=nombre).value=datos["Nombre"]
+            hoja.cell(row=identificador, column=categoria).value=datos["Categoria"]
+            hoja.cell(row=identificador, column=precio).value=datos["Precio"]
+            hoja.cell(row=identificador, column=cantidad).value=datos["Cantidad"] 
+            break
+    Archivo_Exccel.save(rut)
+    return
